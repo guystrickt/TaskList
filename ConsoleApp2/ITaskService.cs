5137c677cfdc0ee70f83ccfab96432f29e11482e
Guystrickt <egrorvegor@gmail.com>
2026-02-26 19:19:38 +0300

    Add files via upload

---
+++ ConsoleApp2/ITaskService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public interface ITaskService
+    {
+        void AddTask(string title);
+        List<TaskItem> GetAllTasks();
+        void MarkTaskAsCompleted(int id);
+        void RemoveTask(int id);
+        TaskItem GetTaskById(int id); 
+    }
+}
---
+++ ConsoleApp2/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class TaskItem
+    {
+        public int Id { get; }
+        public string Title { get; }
+        public bool IsCompleted { get; set; }
+
+        public TaskItem(int id, string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Название задачи не может быть пустым.");
+            }
+            Id = id;
+            Title = title;
+            IsCompleted = false;
+        }
+    }
+}
